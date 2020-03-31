2bd24e924dbd2ee85c6686b9d2ea17de0c36099f
Christofer Laurin <christofer.laurin@gmail.com>
2020-03-31 22:49:43 +0200

    Updates

---
+++ precache-manifest.ec48f8879a3903176efb08d4c3dfe82d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6053eff553bdaf9a28f60d6cdb4922",
+    "url": "/react-webshop/index.html"
+  },
+  {
+    "revision": "bc561ba69fd60ce3a4ff",
+    "url": "/react-webshop/static/css/main.10d06ddb.chunk.css"
+  },
+  {
+    "revision": "afb1ff9999ef4f442095",
+    "url": "/react-webshop/static/js/2.c918ebd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-webshop/static/js/2.c918ebd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc561ba69fd60ce3a4ff",
+    "url": "/react-webshop/static/js/main.de92c9da.chunk.js"
+  },
+  {
+    "revision": "4f54b90de3cf3c7c625e",
+    "url": "/react-webshop/static/js/runtime-main.684b0842.js"
+  },
+  {
+    "revision": "6d0d39ed04816abf6d65d0d63e2f42d6",
+    "url": "/react-webshop/static/media/404.6d0d39ed.jpg"
+  },
+  {
+    "revision": "9a16ef39867068ee9c8cfbb73a815397",
+    "url": "/react-webshop/static/media/404.9a16ef39.mkv"
+  }
+]);
