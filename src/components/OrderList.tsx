7ef194f645ae6cb7f25e95f0de296d10c3477be8
Christofer Laurin <christofer.laurin@gmail.com>
2020-04-07 16:55:30 +0200

    style(OrderAdmin, OrderList) : remove unused useRef

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { getCurrencyFormat } from "../lib/utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconName } from "@fortawesome/fontawesome-svg-core";
