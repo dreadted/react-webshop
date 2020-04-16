0f7724cba149325cbea5a447402b34e33ade80cc
Christofer Laurin <christofer.laurin@gmail.com>
2020-04-16 07:39:02 +0200

    style:(AdminContext) : clean up code

--- src/components/contexts/AdminContext.ts
+++ src/components/contexts/AdminContext.ts
@@ -1,18 +1,5 @@
 import { createContext } from "react";
 
-interface IAdminContext {
-  orderStatusArray: string[];
-  orderStatusColors: string[];
-  statusFilter: number;
-  setStatusFilter: React.Dispatch<React.SetStateAction<number>>;
-  statusMatches: number[];
-  setStatusMatches: React.Dispatch<React.SetStateAction<number[]>>;
-  changeOrderStatus: (status: number, order: Order) => void;
-  updateItem: UpdateItem;
-  saveOrder: (order: Order) => Promise<Order>;
-  deleteOrder: (order: Order) => Promise<Order>;
-}
-
 export const orderStatusArray = [
   "Confirmed",
   "Completed",
@@ -35,26 +22,28 @@ export const orderStatusColors = [
   "#6e5c9d"
 ];
 
-const statusFilter = 0;
-const setStatusFilter = () => {};
-const statusMatches: number[] = [];
-const setStatusMatches = () => {};
-const changeOrderStatus: (status: number, order: Order) => void = () => {};
-const updateItem: UpdateItem = () => {};
-const saveOrder: (order: Order) => Promise<Order> = async order =>
-  new Promise(() => {});
-const deleteOrder: (order: Order) => Promise<Order> = async order =>
-  new Promise(() => {});
+interface IAdminContext {
+  orderStatusArray: string[];
+  orderStatusColors: string[];
+  statusFilter: number;
+  setStatusFilter: React.Dispatch<React.SetStateAction<number>>;
+  statusMatches: number[];
+  setStatusMatches: React.Dispatch<React.SetStateAction<number[]>>;
+  changeOrderStatus: (status: number, order: Order) => void;
+  updateItem: UpdateItem;
+  saveOrder: (order: Order) => Promise<Order>;
+  deleteOrder: (order: Order) => Promise<Order>;
+}
 
 export const AdminContext = createContext<IAdminContext>({
   orderStatusArray,
   orderStatusColors,
-  statusFilter,
-  setStatusFilter,
-  statusMatches,
-  setStatusMatches,
-  changeOrderStatus,
-  updateItem,
-  saveOrder,
-  deleteOrder
+  statusFilter: 0,
+  setStatusFilter: () => {},
+  statusMatches: [],
+  setStatusMatches: () => {},
+  changeOrderStatus: () => {},
+  updateItem: () => {},
+  saveOrder: async order => new Promise(() => {}),
+  deleteOrder: async order => new Promise(() => {})
 });
