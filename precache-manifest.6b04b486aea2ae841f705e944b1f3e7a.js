409aa3c151d9802261c2cc8b115d5974564c0666
Christofer Laurin <christofer.laurin@gmail.com>
2020-04-23 14:24:29 +0200

    Updates

---
+++ precache-manifest.6b04b486aea2ae841f705e944b1f3e7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d93163960cc0bceb33d17fb89d88f43",
+    "url": "/react-webshop/index.html"
+  },
+  {
+    "revision": "65f1c0a1dfd6594dd8ee",
+    "url": "/react-webshop/static/css/main.baf70bd5.chunk.css"
+  },
+  {
+    "revision": "2357360c4f245f4a2a57",
+    "url": "/react-webshop/static/js/2.e763b23d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-webshop/static/js/2.e763b23d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f1c0a1dfd6594dd8ee",
+    "url": "/react-webshop/static/js/main.e907c21d.chunk.js"
+  },
+  {
+    "revision": "4f54b90de3cf3c7c625e",
+    "url": "/react-webshop/static/js/runtime-main.684b0842.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-webshop/precache-manifest.e9987675684b79dc35241802229f7c72.js"
+  "/react-webshop/precache-manifest.6b04b486aea2ae841f705e944b1f3e7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-webshop/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
